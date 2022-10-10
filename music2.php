533e4133c220ac8a73eb74492b7872d050a99ca8
notkub <115391171+notkub@users.noreply.github.com>
2022-10-11 05:58:59 +0700

    Add files via upload

---
+++ music2.php
@@ -0,0 +1,187 @@
+<html>
+<head>
+<title>NotRadio.in.th</title>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<LINK REL="SHORTCUT ICON" href="https://www.notradio.in.th/fav.png">
+<script type="text/javascript">
+function MM_openBrWindow(theURL,winName,features) { //v2.0
+  window.open(theURL,winName,features);
+}
+</script>
+<style>
+::-webkit-scrollbar {
+	width: 7px;
+	height: 7px;
+	}
+	::-webkit-scrollbar-track-piece {
+	background-color: #f3fecc;
+	-webkit-border-radius: 8px;
+	}
+	::-webkit-scrollbar-thumb:horizontal, ::-webkit-scrollbar-thumb:vertical {
+	width: 6px;
+	background-color: #734524;
+	-webkit-border-radius: 5px;
+	}
+
+	::-webkit-scrollbar-thumb:horizontal:hover, ::-webkit-scrollbar-thumb:vertical:hover {
+	background-color: #8e512e;
+	}
+
+</style>
+<style type="text/css">body {cursor: url("https://www.notradio.in.th/images/mouse/point.cur"),auto;}
+ a {cursor: url("https://www.notradio.in.th/images/mouse/link.cur"),auto; }
+</style>
+</head>
+<!--<body bgcolor="#e6ebd7" leftmargin="0" topmargin="0" marginwidth="0" marginheight="0"> -->
+</style>
+<br>
+<center>
+<table id="Table_01" width="726" height="178" border="0" cellpadding="0" cellspacing="0">
+	<tr>
+		<td colspan="3">
+			<img src="images/code2_01.png" width="231" height="13" alt=""></td>
+		<td colspan="5" rowspan="2">
+			<img src="images/code2_02.png" width="370" height="26" alt=""></td>
+		<td rowspan="6">
+			<img src="images/code2_03.png" alt="" width="124" height="160" usemap="#Map2" border="0">
+			<map name="Map2"><area shape="poly" coords="2,34,94,25,102,59,7,80,9,91" href="#" onClick="MM_openBrWindow('http://www.notradio.in.th/radio/postRe','','toolbar=yes,location=yes,menubar=yes,scrollbars=yes,resizable=yes,width=600,height=650')">
+			  <area shape="rect" coords="11,90,103,122" href="javascript:location.reload(true)">
+			  <area shape="rect" coords="6,125,102,156" href="#" onClick="MM_openBrWindow('https://www.notradio.in.th/radio/10ls2','','toolbar=yes,location=yes,menubar=yes,scrollbars=yes,resizable=yes,width=500,height=500')">
+		  </map></td>
+		<td>
+			<img src="images/spacer.gif" width="1" height="13" alt=""></td>
+	</tr>
+	<tr>
+		<td rowspan="4">
+			<img src="images/code2_04.png" width="37" height="132" alt=""></td>
+		<td rowspan="4">
+		<iframe frameborder="no" height="131" scrolling="no" src="https://www.notradio.in.th/radio/imgPJ.php" width="165"></iframe></td>
+		<!--	<img src="https://www.notradio.in.th/radio/images/AutoPJ.png" width="165" height="132" alt=""></td>-->
+		<td rowspan="7">
+			<img src="images/code2_06.png" width="29" height="164" alt=""></td>
+		<td>
+			<img src="images/spacer.gif" width="1" height="13" alt=""></td>
+	</tr>
+	<tr>
+		<td colspan="2">
+			<img src="images/code2_07.png" alt="" width="48" height="113" usemap="#Map" border="0">
+			<map name="Map">
+			  <area shape="rect" coords="9,4,40,42" href="#">
+			  <area shape="rect" coords="9,45,43,78" href="#">
+		  </map></td>
+		<td><iframe frameborder="0" height="113" id="1222" name="s" scrolling="No" src="https://www.notradio.in.th/radio/status/status4.php" width="308"></iframe></td>
+		<td>
+			<img src="images/code2_09.png" width="9" height="113" alt=""></td>
+		<td rowspan="4">
+			<img src="images/code2_10.png" width="5" height="134" alt=""></td>
+		<td>
+			<img src="images/spacer.gif" width="1" height="113" alt=""></td>
+	</tr>
+	<tr>
+		<td colspan="4">
+			<img src="images/code2_11.png" width="365" height="3" alt=""></td>
+		<td>
+			<img src="images/spacer.gif" width="1" height="3" alt=""></td>
+	</tr>
+	<tr>
+		<td rowspan="3">
+			<img src="images/code2_12.png" width="16" height="19" alt=""></td>
+			<td colspan="3" rowspan="3"><audio controls="controls" autoplay style="height:18px; width:348px;" src="https://djstreamsv1.hostpleng.cloud/vFrr"></audio><!-- BEGINS: AUTO-GENERATED MUSES RADIO PLAYER CODE -->
+<!-- <script type="text/javascript" src="https://www.notradio.in.th/radio/js/mrp.js"></script>
+<script type="text/javascript">
+MRP.insert({
+'url':'https://hp1.hostpleng.cloud/9127/;stream.mp3',
+'codec':'mp3',
+'volume':100,
+'autoplay':true,
+'jsevents':true,
+'buffering':0,
+'title':'ฟังเพลงเพราะๆจาก NotRadio.in.th',
+'welcome':'ยินดีต้อนรับทุกคน',
+'wmode':'transparent',
+'skin':'original',
+'width':329,
+'height':19
+});
+</script>
+<!-- ENDS: AUTO-GENERATED MUSES RADIO PLAYER CODE --></td>
+		<td>
+			<img src="images/spacer.gif" width="1" height="3" alt=""></td>
+	</tr>
+	<tr>
+		<td colspan="2" rowspan="3">
+			<img src="images/code2_14.png" width="202" height="32" alt=""></td>
+		<td>
+			<img src="images/spacer.gif" width="1" height="15" alt=""></td>
+	</tr>
+	<tr>
+		<td colspan="2">
+			<img src="images/code2_15.png" width="129" height="1" alt=""></td>
+		<td>
+			<img src="images/spacer.gif" width="1" height="1" alt=""></td>
+	</tr>
+	<tr>
+		<td colspan="6">
+			<img src="images/code2_16.png" width="494" height="16" alt=""></td>
+		<td>
+			<img src="images/spacer.gif" width="1" height="16" alt=""></td>
+	</tr>
+	<tr>
+		<td>
+			<img src="images/spacer.gif" width="37" height="1" alt=""></td>
+		<td>
+			<img src="images/spacer.gif" width="165" height="1" alt=""></td>
+		<td>
+			<img src="images/spacer.gif" width="29" height="1" alt=""></td>
+		<td>
+			<img src="images/spacer.gif" width="16" height="1" alt=""></td>
+		<td>
+			<img src="images/spacer.gif" width="32" height="1" alt=""></td>
+		<td>
+			<img src="images/spacer.gif" width="308" height="1" alt=""></td>
+		<td>
+			<img src="images/spacer.gif" width="9" height="1" alt=""></td>
+		<td>
+			<img src="images/spacer.gif" width="5" height="1" alt=""></td>
+		<td>
+			<img src="images/spacer.gif" width="124" height="1" alt=""></td>
+		<td></td>
+	</tr>
+</table>
+</center>
+<div>
+<div align="center">
+<table border="0" cellpadding="0" cellspacing="0" width="200">
+	<tbody>
+		<tr>
+		</tr>
+		<tr>
+			<td>
+			</div>
+			</div>
+			</td>
+			<td>&nbsp;</td>
+		</tr>
+		<tr>
+			<td>
+			<table style="margin: 0px auto; font-size: 13px; line-height: 18px; font-family: MuseoSans, Arial, sans-serif; width: 620px; color: rgb(74, 72, 66); position: relative; border-collapse: collapse; height: 448px; text-align: center; border-spacing: 0px; max-width: 100%; ">
+				<tbody>
+					<tr>
+					  <td style="padding: 0px; margin: 0px; vertical-align: middle; line-height: normal; "><strong style="color: rgb(139 69 19); font-family: arial, helvetica, sans-serif; font-size: 18px; text-align: -webkit-center;">เพลงที่เล่นไปแล้ว </strong><br />
+					    <iframe frameborder="no" height="680" scrolling="no" src="https://www.notradio.in.th/api/10lsV2.php?type=1" width="240"></iframe></td>
+					</tr>
+				</tbody>
+			</table>
+			</td>
+		</tr>
+		<tr>
+			<td>
+			<div align="center"></div>
+			</td>
+		</tr>
+	</tbody>
+</table>
+</div>
+<!-- End Save for Web Slices -->
+</body>
+</html>
